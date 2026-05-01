e6b9a512ab8748d4b8e21c0efec67dd0e7d06c16
Kraken <moenyi113@gmail.com>
2026-05-01 20:44:39 +0630

    second commit

--- resources/views/authentication/layout/master.blade.php
+++ resources/views/authentication/layout/master.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>POS Admin Dashboard</title>
+    <title>POS Login page</title>
 
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
